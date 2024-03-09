6f5833ffb8362d0fa9798d1de798c8d69320189c
Kaustubh-Narwade <kaustubh.narwade11@gmail.com>
2024-03-09 12:23:20 +0530

    .eslintrc

--- src/app/components/Skill.jsx
+++ src/app/components/Skill.jsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Skill = ({ skillName, skillPath }) => {
   return (
     <div className="flex gap-3 w-fit h-fit bg-[#121212] rounded-xl border border-[#33353F] hover:bg-slate-400 hover:bg-opacity-20  px-5 py-3 mx-4 my-2 duration-[350ms] cursor-default">
       <p className="text-2xl text-[#dedddd]">{skillName}</p>
-      <img className="h-8 w-8" src={skillPath} alt="C++" />
+      <img className="h-8 w-8" src={skillPath} alt={skillName} />
+      {/* <Image
+         
+        src={skillPath}
+        alt={skillName}
+        width={32} 
+        height={32} 
+      /> */}
     </div>
   );
 };
