9d856d6c57e84eeda74454f5fb74c6b22bba07c2
Kaustubh-Narwade <kaustubh.narwade11@gmail.com>
2024-03-14 11:20:31 +0530

    footer font

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-[#fff] cursor-default">
-      <div className="container my-2 flex justify-center gap-1">
-        <p className="text-[#ADB7BE]">created by </p>
-        <p className="text-[#fff]">Kaustubh</p>
+      <div className="container my-2 flex justify-center gap-1 text-[12px] sm:text-sm md:text-lg">
+        <p className="text-[#ADB7BE] ">created by </p>
+        <p className="text-[#fff] ">Kaustubh</p>
       </div>
     </footer>
   );
